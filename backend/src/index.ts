a4eaba47ba3c9f7725fb94e4b1bd154dd0432f70
e_mprysi <maksym_prysiazhnyi@kb.cz>
2020-02-07 16:21:25 +0100

    Work on transformation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import {connectDb} from "./connection";
 import {Node} from "./Node.model";
 import result from './assets/result.json'
 import cors from 'cors'
+import * as _ from 'lodash';
 
 const app = express();
 
@@ -10,10 +11,33 @@ app.use(cors());
 
 const PORT = 8081;
 
+const nodesToTree = (nodes: any) => {
+    const tree = {};
+    for (const node of nodes) {
+        const arr = node.name.split(">");
+        const last = arr.pop();
+        let path = '';
+        for (const lvl of arr) {
+            path += '[' + lvl + ']';
+        }
+        _.set(tree, path + '.name', last);
+    }
+    return tree;
+};
+
 app.get("/nodes", async (req, res) => {
     const nodes = await Node.find();
 
     res.json(nodes);
+});
+
+
+app.get("/nodes-tree", async (req, res) => {
+    const nodes = await Node.find();
+
+    const tree = nodesToTree(nodes);
+    console.log(tree);
+    res.json();
 });
 
 
