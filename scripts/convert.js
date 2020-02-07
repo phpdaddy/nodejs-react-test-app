88cfdc902084613d699a245ae7258ce5a8df1ace
e_mprysi <maksym_prysiazhnyi@kb.cz>
2020-02-07 13:33:35 +0100

    Working with html

--- scripts/convert.js
+++ scripts/convert.js
@@ -9,15 +9,35 @@ require.extensions['.html'] = function (module, filename) {
 
 const input = require("../src/assets/input.html");
 
+function loopTree(prefix, ul) {
+    const lis = ul.querySelectorAll(":scope > li");
+    for (var i = 0; i < lis.length; ++i) {
+        const nodeText = lis[i].querySelector('a').textContent.trim();
+
+        const groups = /(.+)\((.+)\)/.exec(nodeText);
+        const name = groups[1].trim();
+        const size = groups[2].trim();
+
+        const node = {
+            name: prefix ? prefix + " > " + name : name,
+            size: size
+        };
+
+        console.log(node);
+
+        const ul = lis[i].querySelector("ul");
+
+        if (ul) {
+            loopTree(node.name, ul);
+        }
+    }
+}
+
 module.exports.run = function () {
     console.log('hi');
 
     const {window} = new JSDOM(input);
 
-    var ul = window.document.querySelector("ul");
-    console.log(ul.className)
-    var items = ul.querySelectorAll(":scope > li > a");
-    for (var i = 0; i < items.length; ++i) {
-        const nodeText = console.log(items[i].textContent.trim())
-    }
+    loopTree('', window.document.querySelector("ul"));
+
 };
